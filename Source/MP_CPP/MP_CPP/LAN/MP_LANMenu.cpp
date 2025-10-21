6e07aa763044fa90998b3aaa32fa0c1c0d65fda8
Arek <arek0006@gmail.com>
2025-10-21 23:48:02 +0200

    4. LAN Connection

---
+++ Source/MP_CPP/MP_CPP/LAN/MP_LANMenu.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MP_LANMenu.h"
+#include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "Kismet/GameplayStatics.h"
+
+void UMP_LANMenu::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	const FInputModeUIOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(true);
+
+	Button_Host->OnClicked.AddDynamic(this, &ThisClass::HostButtonClicked);
+	Button_Join->OnClicked.AddDynamic(this, &ThisClass::JoinButtonClicked);
+}
+
+void UMP_LANMenu::HostButtonClicked()
+{
+	const FInputModeGameOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(false);
+
+	UGameplayStatics::OpenLevelBySoftObjectPtr(this, HostingLevel, true, TEXT("listen"));
+}
+
+void UMP_LANMenu::JoinButtonClicked()
+{
+	const FInputModeGameOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(false);
+
+	const FString Address{TextBox_IpAddress->GetText().ToString()};
+	UGameplayStatics::OpenLevel(this, *Address);
+}
